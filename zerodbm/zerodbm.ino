09ae3f2bb4ddb17c8e36747a7e6903a93adee3d5
Stewart Bryant <stewart@g3ysx.org.uk>
2014-06-13 14:34:27 +0100

    Sleeps after 10 mins
    Got as far as I can get without a diode.

--- zerodbm/zerodbm.ino
+++ zerodbm/zerodbm.ino
@@ -1,35 +1,47 @@
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include <avr/io.h>
+
 const int RedPin = 3;
 const int GreenPin = 4;
 const int FT = 200;
+const int diodeIn =  A7;
+unsigned long sleepTime;
 
 void setup()
 {
   Serial.begin(9600);
-  //analogReference(DEFAULT);
+  analogReference(INTERNAL);
   pinMode(RedPin, OUTPUT);
   pinMode(GreenPin, OUTPUT);
+  
+  sleepTime = millis() + (unsigned long)(1000)*60*10;
+  Serial.print("sleep time "); Serial.print(sleepTime); Serial.println();
+  
 }
 
 void loop()
 {
    static int adc = 0;
    
+   if (millis() > sleepTime) {
+      Serial.println("going to sleep");
+      ledBlack(FT);
+      delay(1000);
+      redDot(10);
+      Serial.println("goodnight");
+      delay(1000);
+      sleepNow(); 
+   }
+   
+   adc = analogRead(diodeIn);
+   
    Serial.print(adc);
    Serial.println();
 
    ADCtoLed(adc);
-   adc++;
-   
-   //sos();
-  
-   //int diodeIn;
-   
-   //lo = analogRead(diodeIn);
-   
-  
-   //Serial.print(diodeIn);
-   //delay(1000);
-   //Serial.println();
+   delay(1000);
 }
 
 void ADCtoLed(int adc)
@@ -42,47 +54,47 @@ void ADCtoLed(int adc)
    }
    //9	467	r9
    if (adc>=467){
-      redShort(9); 
+      redDotandPause(9); 
       return;
    }
    //8	407	r8
    if (adc>=407){
-      redShort(8);
+      redDotandPause(8);
       return;
    }
    //7	353	r7
    if (adc>=353){
-      redShort(7);
+      redDotandPause(7);
       return;
    }
    //6	306	r6
    if (adc>=306){
-      redShort(6);
+      redDotandPause(6);      
       return;
    }
    //5	267	r5
    if (adc>=267){
-      redShort(5);
+      redDotandPause(5);
       return;
    }
    //4	231	r4
    if (adc>=251){
-      redShort(4);
+      redDotandPause(4);
       return;
    }
    //3	199	r3
    if (adc>=199){
-      redShort(3);
+      redDotandPause(3);
       return;
    }
    //2	170	r2
    if (adc>=170){
-      redShort(2);
+      redDotandPause(2);
       return;
    }
    //1	146	r1
    if (adc>=146){
-      redShort(1);
+      redDotandPause(1);
       return;
    }
    //0	125	gsolid
@@ -92,87 +104,63 @@ void ADCtoLed(int adc)
    }
   //-1	107	g1
      if (adc>=107){
-      greenShort(1);
+      greenDotandPause(9);
       return;
    }
    //-2	91	g2
    if (adc>=91){
-      greenShort(2);
+      greenDotandPause(8);
       return;
    }
    //-3	77	g3
    if (adc>=77){
-      greenShort(3);
+      greenDotandPause(7);
       return;
    }
    //-4	65	g4
    if (adc>=65){
-      greenShort(4);
+      greenDotandPause(6);
       return;
    }
    //-5	55	g5
    if (adc>=55){
-      greenShort(5);
+      greenDotandPause(5);
       return;
    }
    //-6	46	g6
    if (adc>=46){
-      greenShort(6);
+      greenDotandPause(4);
       return;
    }
    //-7	38	g7
    if (adc>=38){
-      greenShort(7);
+      greenDotandPause(3);
       return;
    }
    //-8	32	g8
    if (adc>=32){
-      greenShort(8);
+      greenDotandPause(2);
       return;
    }
    //-9	26	g9
    if (adc>=26){
-      greenShort(9);
+      greenDotandPause(1);
       return;
    }
-   // -10	22	g10
-   if (adc>=22){
-      greenShort(10);
-      return;
-   }
-   //-11	18	g1/1
-   if (adc>=18){
-      greenShort(11);
-      return;
-   }
-   //-12	15	g1/2
-   if (adc>=15){
-      greenShort(12);
-      return;
-   }
-   //-13	12	g1/3
-   if (adc>=12){
-      greenShort(13);
-      return;
-   }
-   //-14	10	g1/4
-   if (adc>=10){
-      greenShort(14);
-      return;
-   }
-   //-15	8	g1/5
-   if (adc>=8){
-      greenShort(15);
-      return;
-   }
-   //-16	6	black
+
+   //-10	22	black
    else{
-      ledGreen(0);
+      ledBlack(0);
       return;
    }
 }
 
 
+void redDotandPause(int n)
+{
+   redDot(n);
+   ledBlack(2*FT); 
+}
 
 void redDot(int n)
 {
@@ -204,6 +192,21 @@ void greenDot(int n)
    }  
 }
 
+void greenDotandPause(int n)
+{
+   greenDot(n);
+   ledBlack(2*FT); 
+}
+
+void greenDash(int n)
+{
+   int i;
+   
+   for (i=0; i < n; i++) {
+      ledGreen(3*FT);
+      ledBlack(FT);
+   }  
+}
 
 
 void ledBlack(int t)
@@ -227,66 +230,27 @@ void ledGreen(int t)
    delay(t);
 }
 
-void redShort(n)
-//for n in range 1..9
-{
-   if ((n < 1) || (n>9))
-      outOfRange("redShort", n);
-
-   if (n > 5) {
-     redDash(1);
-     switch(n){
-       case 9:
-         n = 1; break;
-       case 8:
-         n = 2; break;
-       case 7:
-         n = 3; break;
-       case 6:
-         n = 4; break;
-       default:
-     }
-   }
-   switch(n){
-     case 9:
-     case 8:
-     case 7:
-   
-}
-
-void greenShort(n)
-//for n in range 1..15
-{
-  
-}
-
-void outOfRange()
-      //out of range
-      Serial.print("redShort OOR");
-      Serial.println(n);
-      sos();
-
 void sos()
 {
-   int i;
-   while (true) {
-      for (i=0; i<3; i++){
-         ledRed(FT);
-         ledBlack(FT);
-      }
-      ledBlack(FT);
-      for (i=0; i<3; i++){
-         ledRed(3*FT);
-         ledBlack(FT);
-      }
-      ledBlack(FT);
-      for (i=0; i<3; i++){
-         ledRed(FT);
-         ledBlack(FT);
-      }
-      ledBlack(2*FT);
-      delay(1000);
-   }
- }
+   redDot(3);
+   ledBlack(2*FT);
+   redDash(3);
+   ledBlack(2*FT);
+   redDot(3);
+   ledBlack(2*FT);
+   delay(1000);
+}
 
+void sleepNow(void)
+{
+    //
+    // Choose our preferred sleep mode:
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    //
+    // Set sleep enable (SE) bit:
+    sleep_enable();
+    //
+    // Put the device to sleep:
+    sleep_mode();
+}
 
